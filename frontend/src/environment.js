9fb7e796a9fd7cf0a64a301506d82154e5e20cba
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2025-09-04 19:49:44 +0530

    go

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,14 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://zoomybackend.onrender.com" :
 
     "http://localhost:8000"
 
 
 export default server;
+// let IS_PROD = false;
+// const server = {
+//     dev: "http://localhost:8000",
+//     prod: "https://zoomybackend.onrender.com"
+// }
+// export default server;
